c09f82062834193848a26991abaed873b1de7e8b
12345-dd <mayurpatil98607@gmail.com>
2024-05-31 09:29:21 +0530

    variable declaration

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@
 import './App.css';
 
 function App() {
+  var age = 18;
+  var name = "Mayur";
+  var isMarried = true;
+  var user = {
+    name : "Raj",
+    city : "Surat"
+  }
   return (
     <div className="App">
       {/* <header className="App-header">
@@ -21,6 +28,12 @@ function App() {
       <h1> Welcome to the World of React...</h1>
       <p> Note-1 : Every tag must have closing tag.</p>
       <p> Note-2 : whatever we write in return, it will only show on Browser.</p>
+      <p> Note-3 : At a time we return only one tag.</p>
+      <p> Age = {age}</p>
+      <p> Name = {name}</p>
+      <p>Is Married = {isMarried ? "married" : "un-married"}</p>
+      <p>User Name = {user.name}</p>
+      <p>User City = {user.city}</p>
     </div>
   );
 }
