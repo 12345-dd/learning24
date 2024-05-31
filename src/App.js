1880b46199e153f87b198dbe8db3b8456cc90d61
12345-dd <mayurpatil98607@gmail.com>
2024-05-31 09:05:47 +0530

    Create app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +17,10 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <h1> Welcome to the World of React...</h1>
+      <p> Note-1 : Every tag must have closing tag.</p>
+      <p> Note-2 : whatever we write in return, it will only show on Browser.</p>
     </div>
   );
 }
