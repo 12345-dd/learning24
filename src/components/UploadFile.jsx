2104f5c6433e6e4ce58829841771012e3e11d4b8
12345-dd <mayurpatil98607@gmail.com>
2024-10-25 19:54:05 +0530

    upload files

--- src/components/UploadFile.jsx
+++ src/components/UploadFile.jsx
@@ -1,4 +1,4 @@
-import axios from "axios";
+/*import axios from "axios";
 import React, { useState } from "react";
 
 export const UploadFile = () => {
@@ -35,6 +35,71 @@ export const UploadFile = () => {
         ></img>
       )}
       <br/><br/>
+    </div>
+  );
+};*/
+
+import axios from "axios";
+import React, { useState } from "react";
+
+export const UploadFile = () => {
+  const [name, setName] = useState("");
+  const [file, setFile] = useState(null);
+  const [uploadedItems, setUploadedItems] = useState([]);
+
+  const fileUploadHandler = async () => {
+
+    const formData = new FormData();
+    formData.append("myImage", file);
+    formData.append("name", name);
+
+      const res = await axios.post("http://localhost:4000/upload/upload", formData);
+      console.log("response:", res.data);
+
+      if (res.data && res.data.product && res.data.product.image) {
+        setUploadedItems((prevItems) => [
+          ...prevItems,
+          { name, image: res.data.product.image }
+        ]);
+
+        setName(""); 
+        setFile(null);
+      } else {
+        console.log(res.data);
+      }
+
+  };
+
+  return (
+    <div>
+      <h1>Upload File</h1>
+      <br/>
+      <input
+        type="text"
+        placeholder="Enter name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <br/><br/>
+      <input
+        type="file"
+        id="file"
+        onChange={(e) => setFile(e.target.files[0])}
+      />
+      <br/><br/>
+      <button class="btn btn-primary" onClick={fileUploadHandler}>Add</button>
+
+      <div style={{ display: "flex", flexWrap: "wrap" ,gap: "1rem", marginTop: "1rem" ,justifyContent:"center"}}>
+      {
+        uploadedItems.map((item) => (
+            <div>
+              <h3>{item.name}</h3>
+              <img src={item.image} style={{ width: "300px", height: "300px" }} />
+            </div>
+          
+        ))
+      }
+      </div>
     </div>
   );
 };
